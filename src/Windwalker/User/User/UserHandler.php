cac9ff6ca491764d7a6c665c6e13a65405949edf
Simon Asika <asika32764@gmail.com>
2014-10-10 17:45:58 +0800

    Add UserHandler

---
+++ src/Windwalker/User/User/UserHandler.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Part of starter project. 
+ *
+ * @copyright  Copyright (C) 2014 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\User\User;
+
+use Windwalker\Core\Auth\UserHandlerInterface;
+use Windwalker\Data\Data;
+use Windwalker\DataMapper\DataMapper;
+
+/**
+ * The UserHandler class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class UserHandler implements UserHandlerInterface
+{
+	/**
+	 * Property mapper.
+	 *
+	 * @var DataMapper
+	 */
+	protected $mapper;
+
+	/**
+	 * load
+	 *
+	 * @param array $conditions
+	 *
+	 * @return  mixed
+	 */
+	public function load($conditions)
+	{
+		$user = $this->getDataMapper()->findOne($conditions);
+
+		if ($user->isNull())
+		{
+			return false;
+		}
+
+		return (array) $user;
+	}
+
+	/**
+	 * create
+	 *
+	 * @param Data $data
+	 *
+	 * @return  Data
+	 */
+	public function save(Data $data)
+	{
+		if ($data->id)
+		{
+			$this->getDataMapper()->updateOne($data);
+		}
+		else
+		{
+			$this->getDataMapper()->createOne($data);
+		}
+
+		return $data;
+	}
+
+	/**
+	 * delete
+	 *
+	 * @param array $conditions
+	 *
+	 * @return  boolean
+	 */
+	public function delete($conditions)
+	{
+		return $this->getDataMapper()->delete($conditions);
+	}
+
+	/**
+	 * getDataMapper
+	 *
+	 * @return  DataMapper
+	 */
+	protected function getDataMapper()
+	{
+		if (!$this->mapper)
+		{
+			$this->mapper = new DataMapper('users');
+		}
+
+		return $this->mapper;
+	}
+}
+ 
