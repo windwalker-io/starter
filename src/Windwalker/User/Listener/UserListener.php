45b81a303cdd546f3cc308bf95b86eb591b797e2
Simon Asika <asika32764@gmail.com>
2014-10-10 15:46:55 +0800

    UserListaner

---
+++ src/Windwalker/User/Listener/UserListener.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Part of starter project. 
+ *
+ * @copyright  Copyright (C) 2014 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\User\Listener;
+
+use Windwalker\Event\Event;
+
+/**
+ * The UserListener class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class UserListener
+{
+	/**
+	 * onUserAfterLogin
+	 *
+	 * @param Event $event
+	 *
+	 * @return  void
+	 */
+	public function onUserAfterLogin(Event $event)
+	{
+		show($event);
+	}
+}
+ 
