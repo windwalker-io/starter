836b42b760bb5a8237eafa7e0687a6f0d1fb4798
Simon Asika <asika32764@gmail.com>
2015-07-29 15:38:14 +0800

    Fix some database bug

--- src/Windwalker/Composer/StarterInstaller.php
+++ src/Windwalker/Composer/StarterInstaller.php
@@ -38,6 +38,13 @@ class StarterInstaller
 		$io->write('Install complete.');
 	}
 
+	/**
+	 * Generate secret code.
+	 *
+	 * @param IOInterface $io
+	 *
+	 * @return  void
+	 */
 	protected static function genSecretConfig(IOInterface $io)
 	{
 		$file = __DIR__ . '/../../../etc/config.yml';
@@ -53,6 +60,13 @@ class StarterInstaller
 		$io->write('Auto created secret key.');
 	}
 
+	/**
+	 * Generate database config. will store in: etc/secret.yml.
+	 *
+	 * @param IOInterface $io
+	 *
+	 * @return  void
+	 */
 	protected static function genDatabaseConfig(IOInterface $io)
 	{
 		if (!$io->askConfirmation("\nDo you want to use database? [Y/n]: ", false))
@@ -64,23 +78,23 @@ class StarterInstaller
 		$io->write('Database driver only support pdo mysql now.');
 
 		$driver = 'mysql';
-		$host = $io->ask("Database host [localhost]: ", 'localhost');
+		$host = $io->ask("Database host [127.0.0.1]: ", '127.0.0.1');
 		$name = $io->ask("Database name [acme]: ", 'acme');
 		$user = $io->ask("Database user [root]: ", 'root');
 		$pass = $io->askAndHideAnswer("Database password: ");
-		$prefix = $io->ask("Table prefix [ww_]: ", 'ww_');
+		$prefix = $io->ask("Table prefix [wind_]: ", 'wind_');
 
 		$etc = __DIR__ . '/../../../etc';
 
 		$secret = Yaml::parse(file_get_contents($etc . '/secret.dist.yml'));
 
 		$secret['database'] = array(
-			'driver' => $driver,
-			'host' => $host,
-			'user' => $user,
-			'pass' => $pass,
-			'name' => $name,
-			'prefix' => $prefix
+			'driver'   => $driver,
+			'host'     => $host,
+			'user'     => $user,
+			'password' => $pass,
+			'name'     => $name,
+			'prefix'   => $prefix
 		);
 
 		file_put_contents($etc . '/secret.yml', Yaml::dump($secret));
