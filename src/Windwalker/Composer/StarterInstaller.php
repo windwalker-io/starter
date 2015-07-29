4ec41bec03437bd7da4881b6fecc824b15283f8c
Simon Asika <asika32764@gmail.com>
2015-07-29 15:20:46 +0800

    Update root install process

--- src/Windwalker/Composer/StarterInstaller.php
+++ src/Windwalker/Composer/StarterInstaller.php
@@ -8,7 +8,9 @@
 
 namespace Windwalker\Composer;
 
-use Composer\Script\CommandEvent;
+use Composer\IO\IOInterface;
+use Composer\Script\Event;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * The StarterInstaller class.
@@ -20,27 +22,71 @@ class StarterInstaller
 	/**
 	 * Do install.
 	 *
-	 * @param CommandEvent $event The command event.
+	 * @param Event $event The command event.
 	 *
 	 * @return  void
 	 */
-	public static function install(CommandEvent $event)
+	public static function rootInstall(Event $event)
 	{
-		// $root = getcwd();
-
 		$io = $event->getIO();
 
+		static::genSecretConfig($io);
+
+		static::genDatabaseConfig($io);
+
+		// Complete
+		$io->write('Install complete.');
+	}
+
+	protected static function genSecretConfig(IOInterface $io)
+	{
 		$file = __DIR__ . '/../../../etc/config.yml';
 
 		$config = file_get_contents($file);
 
-		$config = str_replace("'This-token-is-not-safe'", md5(uniqid() . 'WindwalkerHash'), $config);
+		$salt = $io->ask("\nSalt to generate secret [WindwalkerHash]: ", 'WindwalkerHash');
+
+		$config = str_replace("'This-token-is-not-safe'", md5(uniqid() . $salt), $config);
 
 		file_put_contents($file, $config);
 
 		$io->write('Auto created secret key.');
+	}
 
-		// Complete
-		$io->write('Install complete.');
+	protected static function genDatabaseConfig(IOInterface $io)
+	{
+		if (!$io->askConfirmation("\nDo you want to use database? [Y/n]: ", false))
+		{
+			return;
+		}
+
+		$io->write('');
+		$io->write('Database driver only support pdo mysql now.');
+
+		$driver = 'mysql';
+		$host = $io->ask("Database host [localhost]: ", 'localhost');
+		$name = $io->ask("Database name [acme]: ", 'acme');
+		$user = $io->ask("Database user [root]: ", 'root');
+		$pass = $io->askAndHideAnswer("Database password: ");
+		$prefix = $io->ask("Table prefix [ww_]: ", 'ww_');
+
+		$etc = __DIR__ . '/../../../etc';
+
+		$secret = Yaml::parse(file_get_contents($etc . '/secret.dist.yml'));
+
+		$secret['database'] = array(
+			'driver' => $driver,
+			'host' => $host,
+			'user' => $user,
+			'pass' => $pass,
+			'name' => $name,
+			'prefix' => $prefix
+		);
+
+		file_put_contents($etc . '/secret.yml', Yaml::dump($secret));
+
+		$io->write('');
+		$io->write('Database config setting complete.');
+		$io->write('');
 	}
 }
