16986702442a2e6ace2872ee3ede94ac899f7ad3
Simon Asika <asika32764@gmail.com>
2015-08-22 00:39:27 +0800

    Docblock

--- src/Acme/Controller/Cover/GetController.php
+++ src/Acme/Controller/Cover/GetController.php
@@ -2,7 +2,7 @@
 /**
  * Part of Windwalker project.
  *
- * @copyright  Copyright (C) 2014 - 2015 LYRASOFT Taiwan, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2014 - 2015 LYRASOFT. All rights reserved.
  * @license    GNU Lesser General Public License version 3 or later. see LICENSE
  */
 
