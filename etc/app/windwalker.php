a36ac83315de382dd4452666b8b9363f453ae6c2
Simon Asika <asika32764@gmail.com>
2016-06-28 23:23:07 +0800

    Add aliases

--- etc/app/windwalker.php
+++ etc/app/windwalker.php
@@ -80,6 +80,74 @@ return [
 	],
 
 	/*
+	 * Register DI Container Aliases
+	 * -------------------------------------
+	 * If you must override some core classes by your own, replace the target class here.
+	 */
+	'di' => [
+		'aliases' => [
+			// System
+			'application' => \Windwalker\Core\Application\WindwalkerApplicationInterface::class,
+			'app'         => \Windwalker\Core\Application\WindwalkerApplicationInterface::class,
+			'package.resolver' => \Windwalker\Core\Package\PackageResolver::class,
+
+			// Web
+			'input'       => \Windwalker\IO\Input::class,
+			'environment' => \Windwalker\Environment\WebEnvironment::class,
+			'browser'     => \Windwalker\Environment\Browser\Browser::class,
+			'platform'    => \Windwalker\Environment\Platform::class,
+			'uri'         => \Windwalker\Uri\UriData::class,
+
+			// Error
+			'error.handler' => \Windwalker\Core\Error\ErrorManager::class,
+
+			// Logger
+			'logger' => \Windwalker\Core\Logger\LoggerManager::class,
+
+			// Event
+			'dispatcher' => \Windwalker\Core\Event\EventDispatcher::class,
+
+			// Database
+			'database'     => \Windwalker\Database\Driver\AbstractDatabaseDriver::class,
+			'db'           => \Windwalker\Database\Driver\AbstractDatabaseDriver::class,
+			'sql.exporter' => \Windwalker\Core\Database\Exporter\AbstractExporter::class,
+
+			// Router
+			'router' => \Windwalker\Core\Router\CoreRouter::class,
+
+			// Language
+			'language' => \Windwalker\Core\Language\CoreLanguage::class,
+
+			// Renderer
+			'renderer.manager' => \Windwalker\Core\Renderer\RendererManager::class,
+			'renderer'         => \Windwalker\Core\Renderer\RendererManager::class,
+			'package.finder'   => \Windwalker\Core\Renderer\Finder\PackageFinder::class,
+			'widget.manager'   => \Windwalker\Core\Widget\WidgetManager::class,
+
+			// Cache
+			'cache.factory' => \Windwalker\Core\Cache\CacheFactory::class,
+
+			// Session
+			'session' => \Windwalker\Session\Session::class,
+
+			// User
+			'authentication' => \Windwalker\Authentication\Authentication::class,
+			'authorisation'  => \Windwalker\Authorisation\Authorisation::class,
+			'user.manager'   => \Windwalker\Core\User\UserManager::class,
+
+			// CSRF
+			'security.csrf' => \Windwalker\Core\Security\CsrfGuard::class,
+
+			// DateTime
+			'datetime' => \Windwalker\Core\DateTime\DateTime::class,
+
+			// Asset
+			'asset' => \Windwalker\Core\Asset\AssetManager::class,
+			'script.manager' => \Windwalker\Core\Asset\ScriptManager::class
+		]
+	],
+
+	/*
 	 * Path Define
 	 * -------------------------------------
 	 * These paths make our core library works correctly.
