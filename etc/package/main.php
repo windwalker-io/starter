2b508a65bd1811e92283b7bbd658a9a64bcfc67a
Simon Asika <asika32764@gmail.com>
2016-07-01 15:39:44 +0800

    rm main.php

--- etc/package/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Part of phoenix project.
- *
- * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
- * @license    GNU General Public License version 2 or later.
- */
