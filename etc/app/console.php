77e5de80dbbc4f1b6fcef63d34da79b60d36aae0
Simon Asika <asika32764@gmail.com>
2018-02-20 03:20:50 +0800

    Code style

--- etc/app/console.php
+++ etc/app/console.php
@@ -13,58 +13,57 @@ use Windwalker\Utilities\Arr;
  * -------------------------------------
  * Things you config here will be used in console environment.
  */
-return Arr::mergeRecursive(
-    include __DIR__ . '/windwalker.php', [
+return Arr::mergeRecursive(include __DIR__ . '/windwalker.php', [
     /*
-	 * Package Registration
-	 * -------------------------------------
-	 * Register new packages to Application. The key ia package name (alias).
-	 *
-	 * You can use `PackageHelper::getPackage('foo')` to get the packages
-	 * you registered here.
-	 */
-    'packages'  => [
+     * Package Registration
+     * -------------------------------------
+     * Register new packages to Application. The key ia package name (alias).
+     *
+     * You can use `PackageHelper::getPackage('foo')` to get the packages
+     * you registered here.
+     */
+    'packages' => [
         'system' => \Windwalker\SystemPackage\SystemPackage::class,
     ],
 
     /*
-	 * Provider Registration
-	 * -------------------------------------
-	 * Register DI service providers to Container.
-	 *
-	 * If you installed 3rd packages from composer, you may add them to here.
-	 * The packages you want to register into Container must provides a Windwalker
-	 * service provider interface.
-	 *
-	 * You can override the default providers in Windwalker core. Just uncomment
-	 * the line, use same key name and replace provider class by your own.
-	 *
-	 * NOTE: You must familiar about how DI Container working, otherwise you may
-	 *       break your system.
-	 */
+     * Provider Registration
+     * -------------------------------------
+     * Register DI service providers to Container.
+     *
+     * If you installed 3rd packages from composer, you may add them to here.
+     * The packages you want to register into Container must provides a Windwalker
+     * service provider interface.
+     *
+     * You can override the default providers in Windwalker core. Just uncomment
+     * the line, use same key name and replace provider class by your own.
+     *
+     * NOTE: You must familiar about how DI Container working, otherwise you may
+     *       break your system.
+     */
     'providers' => [
-        //		'console'  => \Windwalker\Core\Provider\ConsoleProvider::class,
-        //		'logger'   => \Windwalker\Core\Provider\LoggerProvider::class,
-        //		'event'    => \Windwalker\Core\Provider\EventProvider::class,
-        //		'database' => \Windwalker\Core\Provider\DatabaseProvider::class,
-        //		'lang'     => \Windwalker\Core\Provider\LanguageProvider::class,
-        //		'cache'    => \Windwalker\Core\Provider\CacheProvider::class,
-        //		'session'  => \Windwalker\Core\Provider\SessionProvider::class,
-        //		'auth'     => \Windwalker\Core\Provider\UserProvider::class,
-        //		'security' => \Windwalker\Core\Provider\SecurityProvider::class,
-        //		'mailer'   => \Windwalker\Core\Mailer\MailerProvider::class,
-        //		'mailer_adapter' => \Windwalker\Core\Mailer\SwiftMailerProvider::class,
-        //		'queue'    => \Windwalker\Core\Queue\QueueProvider::class
+//        'console'  => \Windwalker\Core\Provider\ConsoleProvider::class,
+//        'logger'   => \Windwalker\Core\Provider\LoggerProvider::class,
+//        'event'    => \Windwalker\Core\Provider\EventProvider::class,
+//        'database' => \Windwalker\Core\Provider\DatabaseProvider::class,
+//        'lang'     => \Windwalker\Core\Provider\LanguageProvider::class,
+//        'cache'    => \Windwalker\Core\Provider\CacheProvider::class,
+//        'session'  => \Windwalker\Core\Provider\SessionProvider::class,
+//        'auth'     => \Windwalker\Core\Provider\UserProvider::class,
+//        'security' => \Windwalker\Core\Provider\SecurityProvider::class,
+//        'mailer'   => \Windwalker\Core\Mailer\MailerProvider::class,
+//        'mailer_adapter' => \Windwalker\Core\Mailer\SwiftMailerProvider::class,
+//        'queue'    => \Windwalker\Core\Queue\QueueProvider::class
     ],
 
     /*
-	 * Register Commands
-	 * -------------------------------------
-	 * Add your own command object or class here.
-	 *
-	 * Uncomment below to override core commands.
-	 */
-    'console'   => [
+     * Register Commands
+     * -------------------------------------
+     * Add your own command object or class here.
+     *
+     * Uncomment below to override core commands.
+     */
+    'console' => [
         'commands' => [
             //'asset'     => \Windwalker\Core\Asset\Command\AssetCommand::class,
             //'migration' => \Windwalker\Core\Migration\Command\MigrationCommand::class,
@@ -74,32 +73,31 @@ return Arr::mergeRecursive(
     ],
 
     /*
-	 * Load Config Files
-	 * -------------------------------------
-	 * Add extra config file that you can customize your application by more settings.
-	 *
-	 * Use numeric key name to make sure configs has correct ordering to load.
-	 * The bigger number will load later and override the previous, so `stc/secret.yml`
-	 * will be the latest file and override all configs.
-	 */
-    'configs'   => [
+     * Load Config Files
+     * -------------------------------------
+     * Add extra config file that you can customize your application by more settings.
+     *
+     * Use numeric key name to make sure configs has correct ordering to load.
+     * The bigger number will load later and override the previous, so `stc/secret.yml`
+     * will be the latest file and override all configs.
+     */
+    'configs' => [
         // Add something here...
     ],
 
     /*
-	 * Event Listeners
-	 * -------------------------------------
-	 * Add event listeners to event Dispatcher, this function help us inject logic between
-	 * every checkpoint when system running.
-	 *
-	 * You can add a class name by `'foo' => \Namespace\MyListener::class` or new an object.
-	 * We also support callable, use `'onEventName' => ['Class', 'method]` to add callback.
-	 *
-	 * If you want to add priority to control the execution ordering of listeners, use array to config it.
-	 * Example: 'foo' => ['class' => MyListener::class, 'priority' => 300, 'enabled' => boolean]
-	 */
+     * Event Listeners
+     * -------------------------------------
+     * Add event listeners to event Dispatcher, this function help us inject logic between
+     * every checkpoint when system running.
+     *
+     * You can add a class name by `'foo' => \Namespace\MyListener::class` or new an object.
+     * We also support callable, use `'onEventName' => ['Class', 'method]` to add callback.
+     *
+     * If you want to add priority to control the execution ordering of listeners, use array to config it.
+     * Example: 'foo' => ['class' => MyListener::class, 'priority' => 300, 'enabled' => boolean]
+     */
     'listeners' => [
         // Add something here...
     ]
-]
-);
+]);
--- etc/app/windwalker.php
+++ etc/app/windwalker.php
@@ -13,105 +13,105 @@
  */
 return [
     /*
-	 * Package Registration
-	 * -------------------------------------
-	 * Register new packages to Application. The key ia package name (alias).
-	 * 
-	 * You can use `PackageHelper::getPackage('foo')` to get the packages
-	 * you registered here.
-	 */
-    'packages'  => [
+     * Package Registration
+     * -------------------------------------
+     * Register new packages to Application. The key ia package name (alias).
+     *
+     * You can use `PackageHelper::getPackage('foo')` to get the packages
+     * you registered here.
+     */
+    'packages' => [
     ],
 
     /*
-	 * Provider Registration
-	 * -------------------------------------
-	 * Register DI service providers to Container.
-	 *
-	 * If you installed 3rd packages from composer, you may add them to here.
-	 * The packages you want to register into Container must provides a Windwalker
-	 * service provider interface.
-	 *
-	 * You can override the default providers in Windwalker core. Just uncomment
-	 * the line, use same key name and replace provider class by your own.
-	 *
-	 * NOTE: You must familiar about how DI Container working, otherwise you may
-	 *       break your system.
-	 */
-    'providers' => [
-        //		'logger' => \Windwalker\Core\Provider\LoggerProvider::class,
-        //		'event'  => \Windwalker\Core\Provider\EventProvider::class,
+     * Provider Registration
+     * -------------------------------------
+     * Register DI service providers to Container.
+     *
+     * If you installed 3rd packages from composer, you may add them to here.
+     * The packages you want to register into Container must provides a Windwalker
+     * service provider interface.
+     *
+     * You can override the default providers in Windwalker core. Just uncomment
+     * the line, use same key name and replace provider class by your own.
+     *
+     * NOTE: You must familiar about how DI Container working, otherwise you may
+     *       break your system.
+     */
+    'providers' =>[
+        //        'logger' => \Windwalker\Core\Provider\LoggerProvider::class,
+        //        'event'  => \Windwalker\Core\Provider\EventProvider::class,
     ],
 
     /*
-	 * Load Config Files
-	 * -------------------------------------
-	 * Add extra config file that you can customize your application by more settings.
-	 *
-	 * Use numeric key name to make sure configs has correct ordering to load.
-	 * The bigger number will load later and override the previous, so `stc/secret.yml`
-	 * will be the latest file and override all configs.
-	 */
-    'configs'   => [
+     * Load Config Files
+     * -------------------------------------
+     * Add extra config file that you can customize your application by more settings.
+     *
+     * Use numeric key name to make sure configs has correct ordering to load.
+     * The bigger number will load later and override the previous, so `stc/secret.yml`
+     * will be the latest file and override all configs.
+     */
+    'configs' => [
         100 => WINDWALKER_ETC . '/config.yml',
         800 => WINDWALKER_ETC . '/secret.yml',
     ],
 
     /*
-	 * Event Listeners
-	 * -------------------------------------
-	 * Add event listeners to event Dispatcher, this function help us inject logic between
-	 * every checkpoint when system running.
-	 *
-	 * You can add a class name by `'foo' => \Namespace\MyListener::class` or new an object.
-	 * We also support callable, use `'onEventName' => ['Class', 'method]` to add callback.
-	 *
-	 * If you want to add priority to control the execution ordering of listeners, use array to config it.
-	 * Example: 'foo' => ['class' => MyListener::class, 'priority' => 300, 'enabled' => boolean]
-	 */
+     * Event Listeners
+     * -------------------------------------
+     * Add event listeners to event Dispatcher, this function help us inject logic between
+     * every checkpoint when system running.
+     *
+     * You can add a class name by `'foo' => \Namespace\MyListener::class` or new an object.
+     * We also support callable, use `'onEventName' => ['Class', 'method]` to add callback.
+     *
+     * If you want to add priority to control the execution ordering of listeners, use array to config it.
+     * Example: 'foo' => ['class' => MyListener::class, 'priority' => 300, 'enabled' => boolean]
+     */
     'listeners' => [
 
     ],
 
     /*
-	 * Register DI Container Aliases
-	 * -------------------------------------
-	 * If you must override some core classes by your own, replace the target class here.
-	 */
-    'di'        => [
+     * Register DI Container Aliases
+     * -------------------------------------
+     * If you must override some core classes by your own, replace the target class here.
+     */
+    'di' => [
         'aliases' => [
             // System
-            'application'      => \Windwalker\Core\Application\WindwalkerApplicationInterface::class,
-            'app'              => \Windwalker\Core\Application\WindwalkerApplicationInterface::class,
-            'config'           => \Windwalker\Core\Config\Config::class,
+            'application' => \Windwalker\Core\Application\WindwalkerApplicationInterface::class,
+            'app'         => \Windwalker\Core\Application\WindwalkerApplicationInterface::class,
+            'config'      => \Windwalker\Core\Config\Config::class,
             'package.resolver' => \Windwalker\Core\Package\PackageResolver::class,
 
             // Web
-            'input'            => \Windwalker\IO\Input::class,
-            'environment'      => \Windwalker\Environment\WebEnvironment::class,
-            'browser'          => \Windwalker\Environment\Browser\Browser::class,
-            'platform'         => \Windwalker\Environment\Platform::class,
-            'uri'              => \Windwalker\Uri\UriData::class,
+            'input'       => \Windwalker\IO\Input::class,
+            'environment' => \Windwalker\Environment\WebEnvironment::class,
+            'browser'     => \Windwalker\Environment\Browser\Browser::class,
+            'platform'    => \Windwalker\Environment\Platform::class,
+            'uri'         => \Windwalker\Uri\UriData::class,
 
             // Error
-            'error.handler'    => \Windwalker\Core\Error\ErrorManager::class,
+            'error.handler' => \Windwalker\Core\Error\ErrorManager::class,
 
             // Logger
-            'logger'           => \Windwalker\Core\Logger\LoggerManager::class,
+            'logger' => \Windwalker\Core\Logger\LoggerManager::class,
 
             // Event
-            'dispatcher'       => \Windwalker\Core\Event\EventDispatcher::class,
+            'dispatcher' => \Windwalker\Core\Event\EventDispatcher::class,
 
             // Database
-            'database'         => \Windwalker\Database\Driver\AbstractDatabaseDriver::class,
-            'db'               => \Windwalker\Database\Driver\AbstractDatabaseDriver::class,
-            'sql.exporter'     => \Windwalker\Core\Database\Exporter\AbstractExporter::class,
+            'database'     => \Windwalker\Database\Driver\AbstractDatabaseDriver::class,
+            'db'           => \Windwalker\Database\Driver\AbstractDatabaseDriver::class,
+            'sql.exporter' => \Windwalker\Core\Database\Exporter\AbstractExporter::class,
 
             // Router
-            'router'           => \Windwalker\Core\Router\MainRouter::class,
+            'router' => \Windwalker\Core\Router\MainRouter::class,
 
             // Language
-            'language'         => \Windwalker\Core\Language\CoreLanguage::class,
+            'language' => \Windwalker\Core\Language\CoreLanguage::class,
 
             // Renderer
             'renderer.manager' => \Windwalker\Core\Renderer\RendererManager::class,
@@ -120,44 +120,44 @@ return [
             'widget.manager'   => \Windwalker\Core\Widget\WidgetManager::class,
 
             // Cache
-            'cache.manager'    => \Windwalker\Core\Cache\CacheManager::class,
-            'cache'            => \Windwalker\Cache\Cache::class,
+            'cache.manager' => \Windwalker\Core\Cache\CacheManager::class,
+            'cache'         => \Windwalker\Cache\Cache::class,
 
             // Session
-            'session'          => \Windwalker\Session\Session::class,
+            'session' => \Windwalker\Session\Session::class,
 
             // User
-            'authentication'   => \Windwalker\Authentication\AuthenticationInterface::class,
-            'authorisation'    => \Windwalker\Authorisation\AuthorisationInterface::class,
-            'user.manager'     => \Windwalker\Core\User\UserManager::class,
+            'authentication' => \Windwalker\Authentication\AuthenticationInterface::class,
+            'authorisation'  => \Windwalker\Authorisation\AuthorisationInterface::class,
+            'user.manager'   => \Windwalker\Core\User\UserManager::class,
 
             // Security
-            'security.csrf'    => \Windwalker\Core\Security\CsrfGuard::class,
-            'crypt'            => \Windwalker\Crypt\CryptInterface::class,
-            'hasher'           => \Windwalker\Crypt\HasherInterface::class,
+            'security.csrf' => \Windwalker\Core\Security\CsrfGuard::class,
+            'crypt' => \Windwalker\Crypt\CryptInterface::class,
+            'hasher' => \Windwalker\Crypt\HasherInterface::class,
 
             // DateTime
-            'datetime'         => \Windwalker\Core\DateTime\Chronos::class,
+            'datetime' => \Windwalker\Core\DateTime\Chronos::class,
 
             // Asset
-            'asset'            => \Windwalker\Core\Asset\AssetManager::class,
-            'script.manager'   => \Windwalker\Core\Asset\ScriptManager::class,
+            'asset' => \Windwalker\Core\Asset\AssetManager::class,
+            'script.manager' => \Windwalker\Core\Asset\ScriptManager::class,
 
             // Mailer
-            'mailer'           => \Windwalker\Core\Mailer\MailerManager::class,
+            'mailer' => \Windwalker\Core\Mailer\MailerManager::class,
 
             // Queue
-            'queue'            => \Windwalker\Core\Queue\Queue::class,
-            'queue.manager'    => \Windwalker\Core\Queue\QueueManager::class,
+            'queue' => \Windwalker\Core\Queue\Queue::class,
+            'queue.manager' => \Windwalker\Core\Queue\QueueManager::class,
         ]
     ],
 
     /*
-	 * Path Define
-	 * -------------------------------------
-	 * These paths make our core library works correctly.
-	 */
-    'path'      => [
+     * Path Define
+     * -------------------------------------
+     * These paths make our core library works correctly.
+     */
+    'path' => [
         'root'       => WINDWALKER_ROOT,
         'bin'        => WINDWALKER_BIN,
         'cache'      => WINDWALKER_CACHE,
