68cacc5b78daa30a0be6c13648374f2548356b35
Simon Asika <asika32764@gmail.com>
2018-06-19 22:49:50 +0800

    Remove DB operation

--- src/Main/Controller/Page/GetController.php
+++ src/Main/Controller/Page/GetController.php
@@ -9,8 +9,6 @@
 namespace Main\Controller\Page;
 
 use Windwalker\Core\Controller\AbstractController;
-use Windwalker\Core\User\User;
-use Windwalker\DataMapper\DataMapper;
 
 /**
  * Class Get
@@ -31,19 +29,6 @@ class GetController extends AbstractController
     protected function doExecute()
     {
         $view = $this->getView();
-
-        (new DataMapper('main_cover'))->find();
-
-        $db = $this->app->database;
-
-        $query = $db->getQuery(true);
-
-        $query->select('*')
-            ->from('main_cover')
-            ->where('id != :id')
-            ->bind('id', 123);
-
-        $db->setQuery($query)->execute();
 
         return $view->setLayout('index')->render();
     }
