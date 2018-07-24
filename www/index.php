f53860cfc10a5d72b1fbc376cb04f293b28e53f9
Simon Asika <asika32764@gmail.com>
2018-07-25 02:48:39 +0800

    CS

--- www/index.php
+++ www/index.php
@@ -15,7 +15,7 @@ if (!is_file($root . '/vendor/autoload.php')) {
 include $root . '/vendor/autoload.php';
 include_once $root . '/etc/define.php';
 
-$app = new \Windwalker\Web\Application;
+$app = new \Windwalker\Web\Application();
 
 define('WINDWALKER_DEBUG', $app->get('system.debug'));
 
