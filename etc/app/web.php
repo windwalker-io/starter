e1121df4bcc624050a40525a96c5c2436b01e972
Simon Asika <asika32764@gmail.com>
2019-02-03 19:50:19 +0800

    indents

--- etc/app/web.php
+++ etc/app/web.php
@@ -14,17 +14,17 @@ use Windwalker\Utilities\Arr;
  * Things you config here will be used in web environment.
  */
 return Arr::mergeRecursive(include __DIR__ . '/windwalker.php', [
-	/*
-	 * Package Registration
-	 * -------------------------------------
-	 * Register new packages to Application. The key ia package name (alias).
-	 *
-	 * You can use `PackageHelper::getPackage('foo')` to get the packages
-	 * you registered here.
-	 */
-	'packages' => [
-		'main' => \Main\MainPackage::class
-	],
+    /*
+     * Package Registration
+     * -------------------------------------
+     * Register new packages to Application. The key ia package name (alias).
+     *
+     * You can use `PackageHelper::getPackage('foo')` to get the packages
+     * you registered here.
+     */
+    'packages' => [
+        'main' => \Main\MainPackage::class
+    ],
 
     /*
      * Provider Registration
@@ -60,82 +60,82 @@ return Arr::mergeRecursive(include __DIR__ . '/windwalker.php', [
 //        'queue'    => \Windwalker\Core\Queue\QueueProvider::class
     ],
 
-	/*
-	 * Register Routing Files
-	 * -------------------------------------
-	 * If you have more routing files, please add them here. You can also override
-	 * core routing files, use same key name to override it.
-	 */
-	'routing' => [
-		'files' => [
-			'web' => WINDWALKER_ETC . '/routing.php'
-		]
-	],
+    /*
+     * Register Routing Files
+     * -------------------------------------
+     * If you have more routing files, please add them here. You can also override
+     * core routing files, use same key name to override it.
+     */
+    'routing' => [
+        'files' => [
+            'web' => WINDWALKER_ETC . '/routing.php'
+        ]
+    ],
 
-	/*
-	 * Http Middlewares
-	 * -------------------------------------
-	 * Register middlewares to Application, these middleware will execute one by one
-	 * and wrap the core process that you can add your logic before and after main
-	 * execution code.
-	 *
-	 * Use numeric key name to control the execution ordering, the biggest number will
-	 * execute first, and the smaller number will nearer to core logic.
-	 * 
-	 * Uncomment the line below to override core middlewares.
-	 */
-	'middlewares' => [
-		//900  => \Windwalker\Core\Application\Middleware\SessionRaiseMiddleware::class,
-		//800  => \Windwalker\Core\Application\Middleware\RoutingMiddleware::class,
-	],
+    /*
+     * Http Middlewares
+     * -------------------------------------
+     * Register middlewares to Application, these middleware will execute one by one
+     * and wrap the core process that you can add your logic before and after main
+     * execution code.
+     *
+     * Use numeric key name to control the execution ordering, the biggest number will
+     * execute first, and the smaller number will nearer to core logic.
+     *
+     * Uncomment the line below to override core middlewares.
+     */
+    'middlewares' => [
+        //900  => \Windwalker\Core\Application\Middleware\SessionRaiseMiddleware::class,
+        //800  => \Windwalker\Core\Application\Middleware\RoutingMiddleware::class,
+    ],
 
-	/*
-	 * Load Config Files
-	 * -------------------------------------
-	 * Add extra config file that you can customize your application by more settings.
-	 *
-	 * Use numeric key name to make sure configs has correct ordering to load.
-	 * The bigger number will load later and override the previous, so `stc/secret.yml`
-	 * will be the latest file and override all configs.
-	 */
-	'configs' => [
-		// Add something here...
-	],
+    /*
+     * Load Config Files
+     * -------------------------------------
+     * Add extra config file that you can customize your application by more settings.
+     *
+     * Use numeric key name to make sure configs has correct ordering to load.
+     * The bigger number will load later and override the previous, so `stc/secret.yml`
+     * will be the latest file and override all configs.
+     */
+    'configs' => [
+        // Add something here...
+    ],
 
-	/*
-	 * Event Listeners
-	 * -------------------------------------
-	 * Add event listeners to event Dispatcher, this function help us inject logic between
-	 * every checkpoint when system running.
-	 *
-	 * You can add a class name by `'foo' => \Namespace\MyListener::class` or new an object.
-	 * We also support callable, use `'onEventName' => ['Class', 'method]` to add callback.
-	 *
-	 * If you want to add priority to control the execution ordering of listeners, use array to config it.
-	 * Example: 'foo' => ['class' => MyListener::class, 'priority' => 300, 'enabled' => boolean]
-	 */
-	'listeners' => [
-		500 => \Windwalker\SystemPackage\Listener\SystemListener::class
-	],
+    /*
+     * Event Listeners
+     * -------------------------------------
+     * Add event listeners to event Dispatcher, this function help us inject logic between
+     * every checkpoint when system running.
+     *
+     * You can add a class name by `'foo' => \Namespace\MyListener::class` or new an object.
+     * We also support callable, use `'onEventName' => ['Class', 'method]` to add callback.
+     *
+     * If you want to add priority to control the execution ordering of listeners, use array to config it.
+     * Example: 'foo' => ['class' => MyListener::class, 'priority' => 300, 'enabled' => boolean]
+     */
+    'listeners' => [
+        500 => \Windwalker\SystemPackage\Listener\SystemListener::class
+    ],
 
-	/*
-	 * Register Error Handler Classes
-	 */
-	'error' => [
-		'handlers' => [
-			// Uncommnet this line if you need error log support
-			// 'log' => \Windwalker\Core\Error\Handler\ErrorLogHandler::class
-		]
-	],
+    /*
+     * Register Error Handler Classes
+     */
+    'error' => [
+        'handlers' => [
+            // Uncommnet this line if you need error log support
+            // 'log' => \Windwalker\Core\Error\Handler\ErrorLogHandler::class
+        ]
+    ],
 
-	/*
-	 * Register User Auth Handlers
-	 */
-	'user' => [
-		'handler' => \Windwalker\Core\User\NullUserHandler::class,
-		'methods' => [
-		],
-		'policies' => [
-		]
-	]
+    /*
+     * Register User Auth Handlers
+     */
+    'user' => [
+        'handler' => \Windwalker\Core\User\NullUserHandler::class,
+        'methods' => [
+        ],
+        'policies' => [
+        ]
+    ]
 ]);
