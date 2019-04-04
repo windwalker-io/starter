84e6ae64d90476139af096def90ea17989890aba
Simon Asika <asika32764@gmail.com>
2019-04-04 10:53:53 +0800

    Update mail.php

--- etc/conf/mail.php
+++ etc/conf/mail.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * Part of phoenix project.
+ * Part of Windwalker project.
  *
  * @copyright  Copyright (C) 2019 ${ORGANIZATION}.
  * @license    __LICENSE__
  */
 
 return [
-    'enabled' => (bool) (env('MAIL_ENABLED') ?? false),
+    'enabled' => (bool) (env('MAIL_ENABLED') ?: false),
 
     // Auto use this as sender if not provided in runtime.
     'from' => [
-        'name' => 'Windwalker',
-        'email' => 'noreply@windwalker.local'
+        'name' => env('MAIL_FROM_NAME') ?: 'Windwalker',
+        'email' => env('MAIL_FROM_EMAIL') ?: 'noreply@windwalker.local'
     ],
 
     // Transport to send mail (SwiftMailer transport)
-    'transport' => env('MAIL_TRANSPORT'),
+    'transport' => env('MAIL_TRANSPORT') ?: 'php',
 
     // SMTP auth profile.
     'smtp' => [
-        'security' => env('MAIL_SMTP_SECURITY') ?? 'tls',
-        'port' => env('MAIL_SMTP_PORT') ?? 2525,
+        'security' => env('MAIL_SMTP_SECURITY') ?: 'tls',
+        'port' => env('MAIL_SMTP_PORT') ?: 2525,
         'host' => env('MAIL_SMTP_HOST'),
         'username' => env('MAIL_SMTP_USERNAME'),
         'password' => env('MAIL_SMTP_PASSWORD'),
