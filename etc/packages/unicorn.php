47136421af13c850f4f64b4485895058dc4616f8
Simon Asika <asika32764@gmail.com>
2021-08-09 00:06:00 +0800

    unicorn

---
+++ etc/packages/unicorn.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Part of starter project.
+ *
+ * @copyright  Copyright (C) 2021 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+return [
+    'unicorn' => [
+        'enabled' => true,
+
+        'csrf' => [
+            'auto_set_cookie' => true,
+            'cookie_name' => 'XSRF-TOKEN'
+        ],
+
+        'listeners' => [
+            \Windwalker\Core\Asset\AssetService::class => [
+                \Unicorn\Listener\UnicornAssetListener::class
+            ]
+        ],
+
+        'providers' => [
+            \Unicorn\UnicornPackage::class
+        ],
+
+        'file_upload' => [
+            'default' => 'default',
+            'profiles' => [
+                'default' => [
+                    'storage' => 'default',
+                    'accept' => null,
+                ],
+                'image' => [
+                    'storage' => 'default',
+                    'accept' => 'image/*',
+                    'resize' => [
+                        'enabled' => true,
+                        'width' => 1200,
+                        'height' => 1200,
+                        'crop' => false,
+                        'quality' => 85,
+                        'output_format' => null
+                    ],
+                ]
+            ]
+        ]
+    ]
+];
