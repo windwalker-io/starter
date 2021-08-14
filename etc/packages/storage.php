6ae593fdd1f15ce2f0df4b9b210d1228e570d741
Simon Asika <asika32764@gmail.com>
2021-08-14 18:36:29 +0800

    config

---
+++ etc/packages/storage.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Part of starter project.
+ *
+ * @copyright  Copyright (C) 2021 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+use Unicorn\Flysystem\FlysystemFactory;
+use Unicorn\Provider\StorageProvider;
+use Unicorn\Storage\StorageFactory;
+
+return [
+    'storage' => [
+        'default' => 'local',
+
+        's3' => [
+            'default_region' => 'ap-northeast-1'
+        ],
+
+        'providers' => [
+            StorageProvider::class
+        ],
+
+        'factories' => [
+            'instances' => [
+                'local' => fn (StorageFactory $factory) => $factory->localStorage(
+                    [
+                        'path' => env('STORAGE_LOCAL_PATH') ?? 'www/assets/upload'
+                    ]
+                ),
+                's3' => fn (StorageFactory $factory) => $factory->s3Storage(
+                    [
+                        'access_key' => env('AWS_ACCESS_KEY'),
+                        'secret' => env('AWS_SECRET'),
+                        'bucket' => env('AWS_S3_BUCKET'),
+                        'subfolder' => env('AWS_S3_SUBFOLDER'),
+                        'endpoint' => env('AWS_S3_ENDPOINT'),
+                        'region' => env('AWS_S3_REGION')
+                    ]
+                ),
+                'flys3' => fn (StorageFactory $factory) => $factory->flysystem(
+                    fn (FlysystemFactory $factory) => $factory->s3v3Adapter(
+                        [
+                            'access_key' => env('AWS_ACCESS_KEY'),
+                            'secret' => env('AWS_SECRET'),
+                            'bucket' => env('AWS_S3_BUCKET'),
+                            'subfolder' => env('AWS_S3_SUBFOLDER'),
+                            'endpoint' => env('AWS_S3_ENDPOINT'),
+                            'region' => env('AWS_S3_REGION')
+                        ]
+                    )
+                )
+            ],
+        ]
+    ]
+];
