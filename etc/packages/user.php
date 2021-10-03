b8db7dc87ed772728bec90b34918352e5da27b46
Simon Asika <asika32764@gmail.com>
2021-10-03 14:21:14 +0800

    Switch user

--- etc/packages/user.php
+++ etc/packages/user.php
@@ -13,6 +13,7 @@ use Lyrasoft\Luna\Entity\User;
 use Lyrasoft\Luna\Subscriber\AdminSessionSubscriber;
 use Lyrasoft\Luna\Subscriber\RememberMeSubscriber;
 use Lyrasoft\Luna\Subscriber\UserAuthSubscriber;
+use Lyrasoft\Luna\Subscriber\UserSwitchSubscriber;
 use Lyrasoft\Luna\User\Handler\UserHandler;
 use Lyrasoft\Luna\User\Handler\UserHandlerInterface;
 use Lyrasoft\Luna\User\UserService;
@@ -33,7 +34,10 @@ return [
         ],
 
         'listeners' => [
-            AppContext::class => AdminSessionSubscriber::class,
+            AppContext::class => [
+                AdminSessionSubscriber::class,
+                UserSwitchSubscriber::class
+            ],
 
             UserService::class => [
                 UserAuthSubscriber::class,
