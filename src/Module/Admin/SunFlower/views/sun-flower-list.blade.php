8e530dc26ae7da1a686e16ad30d0cf4b8c56525f
Simon Asika <asika32764@gmail.com>
2021-11-04 11:50:33 +0800

    td

--- src/Module/Admin/SunFlower/views/sun-flower-list.blade.php
+++ src/Module/Admin/SunFlower/views/sun-flower-list.blade.php
@@ -49,7 +49,7 @@ $workflow = $app->service(\Unicorn\Workflow\BasicStateWorkflow::class);
             @lang('unicorn.grid.responsive.table.desc')
         </p>
 
-        <div class="grid-table table-responsive">
+        <div class="grid-table table-lg-responsive">
             <table class="table table-striped table-hover">
                 <thead>
                 <tr>
@@ -99,7 +99,7 @@ $workflow = $app->service(\Unicorn\Workflow\BasicStateWorkflow::class);
                         <td>
                             <x-row-checkbox :row="$i" :id="$entity->getId()"></x-row-checkbox>
                         </td>
-                        <th>
+                        <td>
                             <x-state-dropdown color-on="text"
                                 button-style="width: 100%"
                                 use-states
@@ -107,7 +107,7 @@ $workflow = $app->service(\Unicorn\Workflow\BasicStateWorkflow::class);
                                 :id="$entity->getId()"
                                 :value="$item->state"
                             />
-                        </th>
+                        </td>
                         <td>
                             <div>
                                 <a href="{{ $nav->to('sun_flower_edit')->id($entity->getId()) }}">
