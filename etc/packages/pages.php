d3da51b4493a804cd2a847b3acddd6b2fbfa1d43
Simon Asika <asika32764@gmail.com>
2021-11-21 03:58:45 +0800

    pages

--- etc/packages/pages.php
+++ etc/packages/pages.php
@@ -8,3 +8,34 @@
  */
 
 declare(strict_types=1);
+
+use Lyrasoft\Luna\PageBuilder\Addon\Button\ButtonAddon;
+use Lyrasoft\Luna\PageBuilder\Addon\Emptyspace\EmptyspaceAddon;
+use Lyrasoft\Luna\PageBuilder\Addon\Feature\FeatureAddon;
+use Lyrasoft\Luna\PageBuilder\Addon\Image\ImageAddon;
+use Lyrasoft\Luna\PageBuilder\Addon\Text\TextAddon;
+
+return [
+    'pages' => [
+        'addons' => [
+            'text' => TextAddon::class,
+            'image' => ImageAddon::class,
+            'button' => ButtonAddon::class,
+            'feature' => FeatureAddon::class,
+            'emptyspace' => EmptyspaceAddon::class,
+        ],
+        'page_extends' => [
+            'global.body' => 'luna.page.extends.global.body'
+        ],
+        'protects' => [
+            'theme.'
+        ],
+        'styles' => [
+            'font_size_unit' => 'rem',
+            'font_size_scale' => 0.0625
+        ],
+        'templates' => [
+            //
+        ]
+    ],
+];
