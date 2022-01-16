8b1a5b6b415a86912f0e579d8aae7f0234d323c1
Simon Asika <asika32764@gmail.com>
2022-01-17 03:04:14 +0800

    assoc

---
+++ resources/migrations/2022011618130001_AssociationInit.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2022.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace App\Migration;
+
+use Lyrasoft\Luna\Entity\Association;
+use Windwalker\Core\Console\ConsoleApplication;
+use Windwalker\Core\Migration\Migration;
+use Windwalker\Database\Schema\Schema;
+
+/**
+ * Migration UP: 2022011618130001_AssociationInit.
+ *
+ * @var Migration          $mig
+ * @var ConsoleApplication $app
+ */
+$mig->up(
+    static function () use ($mig) {
+        $mig->createTable(
+            Association::class,
+            function (Schema $schema) {
+                $schema->char('type')->length(50);
+                $schema->integer('target_id');
+                $schema->varchar('key');
+                $schema->char('hash')->length(32);
+                $schema->json('params');
+
+                $schema->addIndex('type');
+                $schema->addIndex('key');
+                $schema->addIndex('target_id');
+                $schema->addIndex('hash');
+            }
+        );
+    }
+);
+
+/**
+ * Migration DOWN.
+ */
+$mig->down(
+    static function () use ($mig) {
+        $mig->dropTables(Association::class);
+    }
+);
