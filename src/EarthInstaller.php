f9d211ef05f0967c8dbc35b16b74238a1a4cf722
Simon Asika <asika32764@gmail.com>
2022-02-03 22:20:29 +0800

    add messages

--- src/EarthInstaller.php
+++ src/EarthInstaller.php
@@ -11,21 +11,28 @@ declare(strict_types=1);
 
 namespace App;
 
+use Composer\Script\Event;
+
 /**
  * The EarthInstaller class.
  */
 class EarthInstaller
 {
-    public static function install(): void
+    public static function install(Event $event): void
     {
+        $io = $event->getIO();
         $gitignore = __DIR__ . '/../.gitignore';
 
         $ignore = file_get_contents($gitignore);
 
-        preg_replace('~\# @Dev(.*)\# @EndDev~', '', $ignore);
+        $ignore = preg_replace('~# @Dev(.*)# @EndDev\s+~sm', '', $ignore);
 
         file_put_contents($gitignore, $ignore);
 
+        $io->write('Remove .gitignore dev files.');
+
         unlink(__FILE__);
+
+        $io->write('Remove: ' . __FILE__);
     }
 }
