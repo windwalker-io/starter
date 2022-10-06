6cc9c8fa021c1eaefda89d47ce4ae9623a21d6d4
Simon Asika <asika32764@gmail.com>
2022-10-06 15:29:28 +0800

    fa icon pagination

--- views/layout/pagination/basic-pagination.blade.php
+++ views/layout/pagination/basic-pagination.blade.php
@@ -19,44 +19,32 @@ use Windwalker\Core\Pagination\Pagination;
 $result = $pagination->compile();
 $current = $result->getCurrent();
 
-$mobileNeighbours = 2;
+$mobileNeighbours = 1;
 ?>
 
 @if ($pagination->getPages() >= 1)
     <nav aria-label="navigation">
         <ul class="pagination c-pagination">
             @if ($first = $result->getFirst())
-                <li class="page-item">
+                <li class="page-item page-item--first">
                     <a class="page-link" href="{{ $first }}">
-                        <svg style="height: 1.3em; padding-top: .25em; padding-bottom: .25em"
-                            aria-hidden="true" focusable="false" data-prefix="fas" data-icon="fast-backward"
-                            class="svg-inline--fa fa-fast-backward fa-w-16" role="img"
-                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                            <path fill="currentColor"
-                                d="M0 436V76c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v151.9L235.5 71.4C256.1 54.3 288 68.6 288 96v131.9L459.5 71.4C480.1 54.3 512 68.6 512 96v320c0 27.4-31.9 41.7-52.5 24.6L288 285.3V416c0 27.4-31.9 41.7-52.5 24.6L64 285.3V436c0 6.6-5.4 12-12 12H12c-6.6 0-12-5.4-12-12z"></path>
-                        </svg>
+                        <i class="fa-solid fa-backward-step"></i>
                         {{--<span class="sr-only">First</span>--}}
                     </a>
                 </li>
             @endif
 
             @if ($previous = $result->getPrevious())
-                <li class="page-item">
+                <li class="page-item page-item--previous">
                     <a class="page-link" href="{{ $previous }}">
-                        <svg style="height: 1.3em; padding-top: .25em; padding-bottom: .25em"
-                            aria-hidden="true" focusable="false" data-prefix="fas" data-icon="backward"
-                            class="svg-inline--fa fa-backward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512">
-                            <path fill="currentColor"
-                                d="M11.5 280.6l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2zm256 0l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2z"></path>
-                        </svg>
+                        <i class="fa-solid fa-backward"></i>
                         {{--<span class="sr-only">Previous</span>--}}
                     </a>
                 </li>
             @endif
 
             @if ($less = $result->getLess())
-                <li class="page-item d-none d-md-block">
+                <li class="page-item page-item--less d-none d-md-block">
                     <a class="page-link" href="{{ $less }}">
                         Less
                     </a>
@@ -64,11 +52,15 @@ $mobileNeighbours = 2;
             @endif
 
             @foreach ($result->getPages() as $k => $page)
-                <?php
-                $active = $page->name === Pagination::CURRENT;
-                $distanceClass = abs($page->page - $current->page) > $mobileNeighbours ? 'd-none d-md-block' : '';
-                ?>
-                <li class="page-item {{ $active ? 'active' : $distanceClass }}">
+                    <?php
+                    $active = $page->name === Pagination::CURRENT;
+                    $offset = $page->page - $current->page;
+                    $distanceClass = abs($offset) > $mobileNeighbours ? 'd-none d-md-block' : '';
+                    ?>
+                <li class="page-item page-item--{{ $page->page }} {{ $active ? 'active' : $distanceClass }}"
+                    data-offset="{{ $offset }}"
+                    data-page="{{ $page->page }}"
+                >
                     <a class="page-link" href="{{ $active ? 'javascript://' : $page }}">
                         {{ $page->page }}
                     </a>
@@ -76,7 +68,7 @@ $mobileNeighbours = 2;
             @endforeach
 
             @if ($more = $result->getMore())
-                <li class="page-item d-none d-md-block">
+                <li class="page-item page-item--more d-none d-md-block">
                     <a class="page-link" href="{{ $more }}">
                         More
                     </a>
@@ -84,31 +76,19 @@ $mobileNeighbours = 2;
             @endif
 
             @if ($next = $result->getNext())
-                <li class="page-item">
+                <li class="page-item page-item--next">
                     <a class="page-link" href="{{ $next }}">
                         {{--<span class="sr-only">Next</span>--}}
-                        <svg style="height: 1.3em; padding-top: .25em; padding-bottom: .25em"
-                            aria-hidden="true" focusable="false" data-prefix="fas" data-icon="forward"
-                            class="svg-inline--fa fa-forward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512">
-                            <path fill="currentColor"
-                                d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z"></path>
-                        </svg>
+                        <i class="fa-solid fa-forward"></i>
                     </a>
                 </li>
             @endif
 
             @if ($last = $result->getLast())
-                <li class="page-item">
+                <li class="page-item page-item--last">
                     <a class="page-link" href="{{ $last }}">
                         {{--<span class="sr-only">Last</span>--}}
-                        <svg style="height: 1.3em; padding-top: .25em; padding-bottom: .25em"
-                            aria-hidden="true" focusable="false" data-prefix="fas" data-icon="fast-forward"
-                            class="svg-inline--fa fa-fast-forward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512">
-                            <path fill="currentColor"
-                                d="M512 76v360c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12V284.1L276.5 440.6c-20.6 17.2-52.5 2.8-52.5-24.6V284.1L52.5 440.6C31.9 457.8 0 443.4 0 416V96c0-27.4 31.9-41.7 52.5-24.6L224 226.8V96c0-27.4 31.9-41.7 52.5-24.6L448 226.8V76c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12z"></path>
-                        </svg>
+                        <i class="fa-solid fa-forward-step"></i>
                     </a>
                 </li>
             @endif
