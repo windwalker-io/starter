caaf42802c1620f24abeafcd14857454845c49af
Simon Asika <asika32764@gmail.com>
2022-12-27 18:54:17 +0800

    basic roles

--- resources/seeders/user-seeder.php
+++ resources/seeders/user-seeder.php
@@ -37,7 +37,7 @@ $seeder->import(
         $mapper = $orm->mapper(User::class);
 
         $pass = $password->hash('1234');
-        $basicRole = $accessService->getBasicRole();
+        $basicRoles = $accessService->getBasicRoles();
 
         foreach (range(1, 50) as $i) {
             $item = $mapper->createEntity();
@@ -54,7 +54,7 @@ $seeder->import(
 
             $item = $mapper->createOne($item);
 
-            $accessService->addRolesToUser($item, $basicRole);
+            $accessService->addRolesToUser($item, $basicRoles);
 
             $seeder->outCounting();
         }
