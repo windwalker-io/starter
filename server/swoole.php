9ce16263a65754d898641c94abf7dc161ecf58ec
Simon Asika <asika32764@gmail.com>
2023-01-20 04:13:50 +0800

    swoole wip

--- server/swoole.php
+++ server/swoole.php
@@ -14,10 +14,18 @@ namespace App;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 use Swoole\Http\Server;
+use Symfony\Component\Mime\MimeTypes;
 use Windwalker\Core\Application\WebApplication;
 use Windwalker\Core\Runtime\Runtime;
+use Windwalker\Filesystem\Path;
 use Windwalker\Http\Event\RequestEvent;
 use Windwalker\Http\Server\HttpServer;
+
+use Windwalker\Stream\Stream;
+
+use const Windwalker\Stream\READ_ONLY_FROM_BEGIN;
+
+$_ENV['APP_ENV'] = 'dev';
 
 $root = __DIR__ . '/..';
 
@@ -27,7 +35,7 @@ include $root . '/vendor/autoload.php';
 
 include __DIR__ . '/../etc/define.php';
 
-Runtime::ipBlock(['dev'], env('DEV_ALLOW_IPS'));
+// Runtime::ipBlock(['dev'], env('DEV_ALLOW_IPS'));
 
 Runtime::boot(WINDWALKER_ROOT, __DIR__);
 
@@ -45,7 +53,27 @@ $server->getEventDispatcher()->addDealer($app->getEventDispatcher());
 $server->onRequest(function (RequestEvent $event) use ($app) {
     $req = $event->getRequest();
 
-    show((string) $req->getUri());
+    $path = $req->getUri()->getPath();
+
+    if ($path === '/favicon.ico') {
+        $event->setResponse(\Windwalker\Http\Response\Response::fromString('', 200, []));
+        return;
+    }
+
+    $abPath = $app->path('@public/' . $path);
+
+    if (is_file($abPath)) {
+        $mime = new MimeTypes();
+        [$type] = $mime->getMimeTypes(Path::getExtension($abPath)) ?: ['x-empty'];
+
+        $stream = new Stream($abPath, READ_ONLY_FROM_BEGIN);
+
+        $event->setResponse(
+            (new \Windwalker\Http\Response\Response($stream, 200, []))
+                ->withHeader('Content-Type', $type)
+        );
+        return;
+    }
 
     $event->setResponse($app->execute($req));
 });
