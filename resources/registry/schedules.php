76744fa2132566964658a3fe458f7cfe1d98d4e7
Simon Asika <asika32764@gmail.com>
2023-11-07 16:20:32 +0800

    Auto weekly backup schedule #90

--- resources/registry/schedules.php
+++ resources/registry/schedules.php
@@ -11,3 +11,22 @@ use Windwalker\Core\Schedule\Schedule;
  * @var Schedule           $schedule
  * @var ConsoleApplication $app
  */
+
+$schedule->daily('daily_backup')
+    ->handler(
+        function (ConsoleApplication $app) {
+            $dir = env('SQL_BACKUP_DIR') ?: WINDWALKER_TEMP . '/backups';
+            $dir .= '/daily';
+            $app->runProcess('php windwalker db:export --dir "' . $dir . '" -z');
+        }
+    );
+
+$schedule->task('weekly_backup')
+    ->dayOfWeek(0)
+    ->handler(
+        function (ConsoleApplication $app) {
+            $dir = env('SQL_BACKUP_DIR') ?: WINDWALKER_TEMP . '/backups';
+            $dir .= '/weekly';
+            $app->runProcess('php windwalker db:export --dir "' . $dir . '" -z');
+        }
+    );
