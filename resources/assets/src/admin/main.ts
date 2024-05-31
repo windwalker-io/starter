fef7388a0fef447625199d3a9f081f3f84e6ddb2
Simon Asika <asika32764@gmail.com>
2024-05-31 16:02:46 +0800

    Fix unicorn import

--- resources/assets/src/admin/main.ts
+++ resources/assets/src/admin/main.ts
@@ -1,7 +1,7 @@
 import '@regenerator-runtime';
 import '@unicorn/unicorn.js';
-import { UIBootstrap5 } from '@unicorn/ui/ui-bootstrap5.js';
+import '@unicorn/ui/ui-bootstrap5.js';
 
-u.use(UIBootstrap5);
+u.use(UIBootstrap5.UIBootstrap5);
 
 export default u;
