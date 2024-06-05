e4ed42e08226f906e886a62372606c649da046b8
Simon Asika <asika32764@gmail.com>
2024-06-06 01:55:27 +0800

    remove earth files

---
+++ src/Entity/Acme.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Windwalker\Core\DateTime\Chronos;
+use Windwalker\Core\DateTime\ServerTimeCast;
+use Windwalker\ORM\Attributes\AutoIncrement;
+use Windwalker\ORM\Attributes\CastNullable;
+use Windwalker\ORM\Attributes\Column;
+use Windwalker\ORM\Attributes\CreatedTime;
+use Windwalker\ORM\Attributes\PK;
+use Windwalker\ORM\Attributes\Table;
+use Windwalker\ORM\EntityInterface;
+use Windwalker\ORM\EntityTrait;
+
+/**
+ * The Acme class.
+ */
+#[Table('acmes', 'acme')]
+#[\AllowDynamicProperties]
+class Acme implements EntityInterface
+{
+    use EntityTrait;
+
+    #[Column('id'), PK, AutoIncrement]
+    protected ?int $id = null;
+
+    #[Column('title')]
+    protected string $title = '';
+
+    #[Column('content')]
+    protected string $content = '';
+
+    #[Column('created_by')]
+    protected int $createdBy = 0;
+
+    #[Column('created')]
+    #[CastNullable(ServerTimeCast::class)]
+    #[CreatedTime]
+    protected ?Chronos $created = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): int
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(int $createdBy): static
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getCreated(): ?Chronos
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface|string|null $created): static
+    {
+        $this->created = Chronos::tryWrap($created);
+
+        return $this;
+    }
+}
