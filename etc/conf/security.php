aa291f42bde5093fef5d4e80bef84087642ce7d2
Simon Asika <asika32764@gmail.com>
2025-06-04 20:03:08 +0800

    Use di tags

--- etc/conf/security.php
+++ etc/conf/security.php
@@ -2,13 +2,11 @@
 
 declare(strict_types=1);
 
-use Windwalker\Core\Manager\CryptoManager;
 use Windwalker\Core\Manager\HasherManager;
+use Windwalker\Core\Provider\SecurityProvider;
 use Windwalker\Crypt\Hasher\Hasher;
-use Windwalker\Crypt\Hasher\HasherInterface;
 use Windwalker\Crypt\Hasher\PasswordHasher;
 use Windwalker\Crypt\Hasher\PasswordHasherInterface;
-use Windwalker\Crypt\Symmetric\CipherInterface;
 use Windwalker\Crypt\Symmetric\OpensslCipher;
 use Windwalker\Crypt\Symmetric\SodiumCipher;
 use Windwalker\DI\Container;
@@ -38,15 +36,11 @@ return [
         ],
     ],
 
+    'providers' => [
+        SecurityProvider::class,
+    ],
+
     'bindings' => [
-        CryptoManager::class,
-        HasherManager::class,
-        CipherInterface::class => static function (Container $container) {
-            return $container->get(CryptoManager::class)->get();
-        },
-        HasherInterface::class => static function (Container $container) {
-            return $container->get(HasherManager::class)->get();
-        },
         PasswordHasherInterface::class => static function (Container $container) {
             return $container->get(HasherManager::class)->get('password');
         },
