f3458310352ab481d6ef6dcd4dec2bba2369397f
Simon Asika <asika32764@gmail.com>
2025-09-11 16:23:49 +0800

    misc updates

---
+++ resources/registry/enqueuers.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enqueuer;
+
+use Windwalker\Core\Console\ConsoleApplication;
+use Windwalker\ORM\ORM;
+use Windwalker\Queue\Enqueuer;
+use Windwalker\Queue\Enqueuer\EnqueuerController;
+
+/**
+ * @var Enqueuer           $enqueuer
+ * @var ConsoleApplication $app
+ */
+
+// $enqueuer->default(
+//     function (EnqueuerController $controller, ORM $orm) {
+//         //
+//     }
+// );
