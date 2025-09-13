c37f98f4afd9596d2f554f79994bf35231ab07df
Simon Asika <asika32764@gmail.com>
2025-09-13 17:51:26 +0800

    Vite wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
-import laravel from 'laravel-vite-plugin';
+import { expandGlobs, findModules } from '@windwalker-io/core';
+import Crypto from 'crypto';
+import { globSync } from 'fast-glob';
+import { readFileSync } from 'fs';
+import { parse } from 'node-html-parser';
+import { basename, relative, resolve } from 'path';
 import { defineConfig } from 'vite';
+import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 export default defineConfig({
   build: {
+    manifest: true,
     rollupOptions: {
       input: {
-        'loader': 'core-loader',
-        'js/app': 'resources/assets/src/front/main.ts',
-        'css/main': 'resources/assets/scss/front/main.scss',
+        'main': 'main',
+        'js': 'resources/assets/src/front/main.ts',
       },
       output: {
         entryFileNames: (chunkInfo) => {
-          console.log(chunkInfo);
+          // console.log(chunkInfo);
           return '[name].js';
         },
         chunkFileNames: (chunkInfo) => {
-          console.log(chunkInfo);
+          // console.log(chunkInfo);
           return '[name].js';
         },
         assetFileNames: (assetInfo) => {
@@ -28,22 +34,96 @@ export default defineConfig({
     minify: false,
     emptyOutDir: true,
   },
+  optimizeDeps: {
+    // Supress error
+    exclude: ['@windwalker-io/unicorn']
+  },
   plugins: [
+    sassGlobImports(),
     {
-      name: 'v',
+      name: 'merge-module-scss',
+      enforce: 'pre',
       resolveId(id) {
-        if (id === 'core-loader') {
-          return id;
+        if (id === 'main') {
+          return 'resources/assets/scss/front/main.scss';
         }
       },
-      load(id) {
-        if (id === 'core-loader') {
-          return `
-            console.log('WEERT');
-          `;
+
+      async load(id) {
+        if (id !== 'resources/assets/scss/front/main.scss') {
+          return null;
         }
-      }
+
+        const patterns = [
+          ...expandGlobs(findModules('Front/**/assets/*.scss')),
+          ...expandGlobs('src/Module/Front/**/assets/*.scss')
+        ].map(file => resolve(file));
+
+        const imports = patterns
+          .map((pattern) => `@import "${pattern.replace(/\\/g, '/')}";`)
+          .concat(parseStylesFromBlades('src/Module/Front/**/*.blade.php'))
+          .join('\n');
+
+        let main = readFileSync('resources/assets/scss/front/main.scss', 'utf-8');
+
+        main += `\n\n${imports}\n`;
+
+        return main;
+
+        // // 使用絕對路徑（posix），讓 Sass 能直接載入實體檔案
+        // const mainAbs = toPosix(path.resolve(root, main));
+        // const importLines = [`@import "${mainAbs}";`, '/* auto-generated module imports */'];
+        //
+        // for (const f of files) {
+        //   const abs = toPosix(path.resolve(root, f));
+        //   importLines.push(`@import "${abs}";`);
+        // }
+        //
+        // return importLines.join('\n') + '\n';
+      },
     }
   ]
 });
 
+function parseStylesFromBlades(patterns: string | string[]) {
+  let files = globSync(patterns);
+  
+  return files.map((file) => {
+    const bladeText = readFileSync(file, 'utf8');
+
+    const html = parse(bladeText);
+
+    return html.querySelectorAll('style[type],script[type]')
+      .filter(
+        (el) => ['text/scss', 'text/css'].includes(el.getAttribute('type'))
+      )
+      .map((el) => {
+        const scope = el.getAttribute('data-scope');
+        
+        if (scope) {
+          return `${scope} {
+          ${el.innerHTML}
+        }`;
+        } else {
+          return el.innerHTML;
+        }
+      });
+  })
+    .filter((c) => c.length > 0)
+    .flat();
+}
+
+function fileNameHash(bufferOrString: Crypto.BinaryLike, short = 8) {
+  return Crypto.createHash('sha1')
+    .update(bufferOrString)
+    .digest('hex')
+    .substring(0, short);
+}
+
+function getStyleScope(fullLayout: string): string {
+  const relativePath = relative(process.cwd(), fullLayout);
+  const segments = basename(relativePath).split('.');
+  const layoutName = segments.shift() || '';
+
+  return layoutName + '-' + fileNameHash(relativePath, 8);
+}
